485edbf65efc092bf6a925bfed6231739ecd8b4e
trungnghia-coder <nguyentrunghia0907@gmail.com>
2026-06-09 11:47:29 +0700

    feat: add useAuth method

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,20 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "@/lib/firebase/auth";
+
+export function useAuth() {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (u) => {
+      setUser(u);
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
+
+  return { user, loading };
+}
