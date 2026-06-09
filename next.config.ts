2885e4837521ccca45f7e64638e3bb20293af991
trungnghia-coder <nguyentrunghia0907@gmail.com>
2026-06-09 17:56:13 +0700

    fix: disable turnbopack in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  bundlePagesRouterDependencies: true,
+  serverExternalPackages: ["firebase-admin"],
 };
 
 export default nextConfig;
